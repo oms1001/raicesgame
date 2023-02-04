6af9ab5031c0446979b9377bb541d900d5d477c8
oms1001 <oms_100@hotmail.com>
2023-02-03 22:41:43 -0400

    Actualización de Unity

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -11,47 +11,31 @@ using UnityEngine.EventSystems;
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 
 {
-
     [SerializeField] private Canvas canvas;
-
-
-
-
-
     private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
 
     private void Awake()
 
     {
-
         rectTransform = GetComponent<RectTransform>();
-
+        canvasGroup = GetComponent<CanvasGroup>();
     }
-
-
-
-
-
-
 
     public void OnBeginDrag(PointerEventData eventData)
 
     {
-
         Debug.Log("OnBeginDrag");
-
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
     }
 
 
 
     public void OnDrag(PointerEventData eventData)
-
     {
-
         Debug.Log("OnDrag");
-
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
-
     }
 
 
@@ -59,21 +43,15 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     public void OnEndDrag(PointerEventData eventData)
 
     {
-
         Debug.Log("OnEndDrag");
-
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
     }
 
 
 
     public void OnPointerDown(PointerEventData eventData)
-
     {
-
         Debug.Log("OnPointerDown");
-
     }
-
-
-
 }
